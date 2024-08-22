c0d0d868eb60503d94ecdc9c75a274dd65a73934
kotagiri484 <kotagiriakshay6@gmail.com>
2024-08-22 20:00:51 +0530

    practice js script

---
+++ mypractice.js
@@ -0,0 +1,55 @@
+let add =[1,2,34,5,6]
+add.push(7)
+console.log(add)
+
+
+let data =[1,5,6,8,9]
+data.pop()
+console.log(data)
+
+
+let file=[1,5,6,8,104]
+file.unshift(2)
+console.log(file)
+
+
+let dump=[4,5,4,7,8]
+dump.shift()
+console.log(dump)
+
+
+let doc=[1,2,3,4,5,9,10,2,5,6]
+data1=doc.slice(3,8)
+console.log(data1)
+
+
+let bin=[1,3,5,6,8,9,0,12,3,4]
+bin.splice(3,4,77,88)
+console.log(bin)
+
+
+let remove=[1,2,3,5,6,7,8,9]
+delete remove[6]
+console.log(remove)
+
+let bubble=[1,3,5,55,12,13,4,5,6,7,10]
+let my=bubble.sort((a,b)=>a-b)
+console.log(my)
+
+let name ="hello this my world"
+name=name.split("").reverse().join("")
+console.log(name)
+
+
+let gap="    hello world     "
+gap1=gap.trim()
+console.log(gap1)
+
+let change="every man as a day"
+change1=change.replace("man","dog")
+console.log(change1)
+
+
+let close =[1,2,3,5,6,7,6,7,8,9,10,11,12]
+close.fill(3,5)
+console.log(close)
